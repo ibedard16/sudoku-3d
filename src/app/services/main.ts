82529025566d075e1cdd737e0cfff14f74f7565a
Isaac Bedard <ibedard@renovo1.com>
2024-10-12 09:10:44 -0400

    Remove stray swap message I left in
    I was debugging the `swapColumnPlane` method after I wrote it... this doesn't need to be here. Holistically it doesn't matter, since it's right after the shuffle step... but I'm trying to keep my code clean so 🔥

--- src/app/services/main.ts
+++ src/app/services/main.ts
@@ -8,7 +8,6 @@ for (let shuffleStep = 0; shuffleStep < 100; ++shuffleStep) {
   answerKey.shuffle();
 }
 
-answerKey.swapColumnPlane(0, 1);
 postMessage(answerKey.clone().get());
 
 console.log('Answer key is valid?', IsValidGrid(answerKey));
